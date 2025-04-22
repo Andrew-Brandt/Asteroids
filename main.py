7bc3e69dd2855db367411fca5c322700fd0013b2
Andy Brandt <andy.n.brandt@gmail.com>
2025-04-22 10:00:51 -0700

    add rotation

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from player import *
 
 
 def main():
-    print(f"Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+	print(f"Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
-    #game loop
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            
-        player.draw(screen)
-        pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60) / 1000.0
+	#game loop
+	while True:
+		dt = clock.tick(60) / 1000.0
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		# game logic    
+		
+		
+		player.update(dt)      
+
+		# render
+		screen.fill((0, 0, 0))  # Clear the screen with black
+		player.draw(screen)
+	   
+		pygame.display.flip()
+		clock.tick(60)
+		
 
 
 
@@ -31,4 +40,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- player.py
+++ player.py
@@ -23,3 +23,17 @@ class Player(CircleShape):
         # Draw the player as a triangle
         points = self.triangle()
         pygame.draw.polygon(screen, (255, 255, 255), points, width=2) 
+
+    
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+
+ 
