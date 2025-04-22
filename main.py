61a0d491fb7df0d77f386330c03a2ef64afbf5d4
Andy Brandt <andy.n.brandt@gmail.com>
2025-04-22 08:43:22 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+
+
+def main():
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
